17cf14e367dbe5325475dd2db5d6df49e1594399
irfan-fauzi <irfanwintech@yahoo.com>
2025-02-13 00:40:19 +0700

    sample

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
+  const t = "s";
   return (
-    <>
-      <h1>home page</h1>
-    </>
+    <div className='bg-red-500'>
+      <h1 className='text-lg'>home page</h1>
+    </div>
   );
 }
