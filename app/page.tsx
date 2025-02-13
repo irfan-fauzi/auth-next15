347ffc5c0dded6b35c681560e643aa3a0ca903cf
irfan-fauzi <irfanwintech@yahoo.com>
2025-02-14 00:35:23 +0700

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
-  const t = "s";
   return (
     <div className='bg-red-500'>
-      <h1 className='text-lg'>home page</h1>
+      <h1 className='text-lg'>home page </h1>
     </div>
   );
 }
