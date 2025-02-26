1807fdb97491c62ffd6de0b06fb31cc033dae6b0
irfan-fauzi <irfanwintech@yahoo.com>
2025-02-27 00:17:01 +0700

    berhasil menangkap session dari authjs dan menampilkan nama sesuai user setelah login

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,18 @@
+import { auth } from "@/auth";
 
+const DashboardPage = async () => {
+  const session = await auth();
 
-const DashboardPage = () => {
   return (
-    <div>DashboardPage</div>
-  )
-}
+    <>
+      <header className='bg-gray-800 w-full h-[70px] items-center flex'>
+        <nav className='max-w-screen-xl w-full mx-auto flex  items-center justify-between'>
+          <h1 className='text-3xl text-gray-50'>Dashboard</h1>
+          <p className='text-white'>{session?.user?.name}</p>
+        </nav>
+      </header>
+    </>
+  );
+};
 
-export default DashboardPage
+export default DashboardPage;
