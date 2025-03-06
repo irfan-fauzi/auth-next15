2a1aacf82a7405c3e8ee1983edc2b6232955eb31
irfan-fauzi <irfanwintech@yahoo.com>
2025-03-07 00:30:51 +0700

    always

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Hero from "@/ui/components/landing/Sections/Hero";
 import Header from "@/ui/components/landing/Sections/Header";
 import Layout from "@/ui/components/landing/Layout";
@@ -11,7 +12,16 @@ const Home = () => {
       <Hero />
       <Layout>
         <Features />
-        <Gradient width={400} height={400} className="absolute top-28 right-0 inline-block"/>
+        <Gradient width={400} height={400} className="absolute top-28 right-0 inline-block" />
+        <section className="bg-white max-w-screen-xl flex mx-auto">
+          <div className="bg-blue-900">
+            <Image src='/app.webp' width={400} height={200} alt="tampilan aplikasi azfira kasir" />
+          </div>
+          <div>
+            <h2>Why you should choose Azfira Kasir</h2>
+            <p>becouse we provide what you need</p>
+          </div>
+        </section>
       </Layout>
     </>
   );
